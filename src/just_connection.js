597198fd297f5ce7c38a9fea4682404d01925b01
Tom Victor <vjtomvictor@icloud.com>
2017-03-06 15:00:15 +0000

    connection ok code added

---
+++ src/just_connection.js
@@ -0,0 +1,41 @@
+var async = require('async');
+var noble = require('noble');
+
+//var peripheralIdOrAddress = process.argv[2].toLowerCase();
+var peripheralIdOrAddress = "b0:b4:48:d0:b4:03"
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  if (peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
+    noble.stopScanning();
+
+    console.log('peripheral with ID ' + peripheral.id + ' found');
+    var advertisement = peripheral.advertisement;
+
+    var localName = advertisement.localName;
+    var txPowerLevel = advertisement.txPowerLevel;
+    var manufacturerData = advertisement.manufacturerData;
+    var serviceData = advertisement.serviceData;
+    var serviceUuids = advertisement.serviceUuids;
+
+    peripheral.on('disconnect', function() {
+    process.exit(0);
+    });
+
+    peripheral.connect(function (err) {
+      // body...
+      console.log('connected to peripheral: ' + peripheral.uuid);
+    })
+
+    console.log();
+
+
+  }
+});
+
