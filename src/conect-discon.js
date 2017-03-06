dc31d0d11d7dc4e74e186326aa0756315f445589
Tom Victor <vjtomvictor@icloud.com>
2017-03-06 15:28:33 +0000

    update

--- src/conect-discon.js
+++ src/conect-discon.js
@@ -6,24 +6,29 @@ var peripheralIdOrAddress = "b0:b4:48:d0:b4:03"
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     noble.startScanning();
+    console.log('scanning started due to stateChange')
   } else {
     noble.stopScanning();
+    console.log('scanning stopped due to state stateChange')
   }
 });
 
 
 noble.on('discover', function(peripheral) {
 
+  console.log('on discover function')
   if (peripheral.id === peripheralIdOrAddress || peripheral.address === peripheralIdOrAddress) {
     noble.stopScanning();
+    console.log('scanning stopped, got the required device')
 
 
     peripheral.connect(function(error) {
     console.log('connected to peripheral: ' + peripheral.uuid);
+    console.log('connected to peripheral (ID): ' + peripheral.id);
 
     peripheral.disconnect(function(error) {
     console.log('disconnected from peripheral: ' + peripheral.uuid);
-    
+
     });
   });
 
