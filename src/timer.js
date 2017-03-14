5c8f90dd85236b5ee5047d7d25c815fffcc9a108
Tom Victor <vjtomvictor@icloud.com>
2017-03-14 12:35:31 +0000

    more changes

--- src/timer.js
+++ src/timer.js
@@ -1,6 +1,34 @@
 // Blinking interval in usec
 var configTimeout = 4000;
 
+var mqtt = require('mqtt');
+var options = {
+    port: 8883
+};
+var client = mqtt.connect('mqtt://technoripio.cloudapp.net', options);
+
+
+client.on('connect', function () {
+    client.subscribe('tracker')
+    client.publish('log/beacon', 'Hello mqtt')
+})
+
+
+client.on('message', function (topic, message) {
+
+    console.log("entered  switch, with topic" + topic);
+    console.log("message over mqtt:" + message);
+    switch (topic) {
+    case 'tracker':
+	console.log("entered switch case tracker");
+	console.log("");
+    case 'log/beacon':
+	console.log("entered switch case beacon");
+	console.log("");
+    }
+
+});
+
 console.log("Code started");
 
 setInterval(function() {
