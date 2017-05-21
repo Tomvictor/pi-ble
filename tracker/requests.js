12eeadd3bb13cb2bd169c9c187c500d0293ea266
Tom Victor <vjtomvictor@icloud.com>
2017-05-21 12:37:31 +0000

    requests update

--- tracker/requests.js
+++ tracker/requests.js
@@ -1,5 +1,7 @@
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
+var url = 'http://kranioz.com' ;
+var tet = 'http://kranioz.com/ble?range=1';
+request(tet, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
