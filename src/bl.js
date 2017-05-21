e31c48729a166a7092252c5adf41b62fb897e1f3
Tom Victor <vjtomvictor@icloud.com>
2017-05-21 09:32:42 +0000

    new launchpad mac id added

--- src/bl.js
+++ src/bl.js
@@ -2,7 +2,7 @@ var async = require('async');
 var noble = require('noble');
 
 //var peripheralIdOrAddress = process.argv[2].toLowerCase();
-var peripheralIdOrAddress = "b0:b4:48:d0:b4:03"
+var peripheralIdOrAddress = "cc:78:ab:87:b1:81"
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
     noble.startScanning();
